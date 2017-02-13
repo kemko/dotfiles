2707fd5ef9a1192cee2714f8486205efc42fb3b6
Dmitriy Andreev <me@kemko.ru>
2017-02-13 13:08:50 +0300

    byobu package

--- init.sh
+++ init.sh
@@ -24,7 +24,7 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 SOFTWARE="$SOFTWARE sublime-text-installer"
 
 # All other stuff from standard repos
-SOFTWARE="$SOFTWARE whois traceroute tcptraceroute iotop htop mosh gpa meld git zsh python-pip curl httpie xclip grc php7.0-cli php-codesniffer"
+SOFTWARE="$SOFTWARE byobu whois traceroute tcptraceroute iotop htop mosh gpa meld git zsh python-pip curl httpie xclip grc php7.0-cli php-codesniffer"
 
 # Work dependences
 SOFTWARE="$SOFTWARE cmake ccache libssl-dev libreadline-dev zlib1g-dev build-essential ruby-appraiser-rubocop graphicsmagick-libmagick-dev-compat libmagickwand-dev libidn11-dev libldap2-dev libsasl2-dev libxml2-dev libxslt1-dev"
